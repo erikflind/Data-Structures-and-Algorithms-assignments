b34b08280545f7fec82fe2703ef3de4dbdf20e6d
Erik <elindstr@student.chalmers.se>
2023-01-20 15:28:02 +0100

    Recursive firstIndexOf-method with correct complexity ?? B)

--- BinarySearch.java
+++ BinarySearch.java
@@ -9,7 +9,6 @@ public class BinarySearch {
 
   // Check if the array `a` contains the given search key.
   public static <T> boolean contains(T[] a, T key, Comparator<T> comparator) {
-
     int low = 0;
     int high = a.length - 1;
 
@@ -33,55 +32,26 @@ public class BinarySearch {
   public static <T> int firstIndexOf(T[] a, T key, Comparator<T> comparator) {
     int low = 0;
     int high = a.length - 1;
+    int keyIndex = -1; // default return value if the key is not found
 
-    while (low <= high) {
+    return helperFunction(a, key, comparator, low, high, keyIndex);
+  }
+
+  public static <T> int helperFunction(T[] a, T key, Comparator<T> comparator, int low, int high, int keyIndex) {
+    if (!(low > high)) {
       int mid = (low + high) / 2;
       int comparisonValue = comparator.compare(a[mid], key);
 
       if (comparisonValue == 0) {
-        return helperFunction(a, key, comparator, low, mid); // if not perfect try to pass (mid - 1);
-
-          // [1, 2, 3, (3), 3, 4, 5]
-          // [1, (2), 3, 3]
-          // [3, 3]
-
-          // [1, 2, 2, 3, (3), 3, 4, 5, 6]
-          // [1, 2, (2), 3, 3]
-          // [(3), 3]
-
-          // if ( a[low] == key ) ??
-          // or is it: if (a[0] == key] ) ?? // no it has to be a[low] in case you check the top half of the array
-
-          // [1, 3, 3, 5, 6]
-          // [1, 3, 3]
-          // [1, 3]
-          // if low == mid ??
-
-      } else if (comparisonValue < 0) { // key is in upper half of array
-        low = mid + 1;
-      } else { // (comparisonValue > 0)
-        high = mid - 1; // key is in lower half of array
-      }
-    }
-
-    return -1;
-  }
-
-  public static <T> int helperFunction(T[] a, T key, Comparator<T> comparator, int low, int high) {
-    int initialKeyIndex = high;
-    while (!(low == high)) {
-      int mid = (low + high) / 2; // ????????
-      int comparisonValue = comparator.compare(a[mid], key);
-      if (comparisonValue == 0) {
-        return helperFunction(a, key, comparator, low, mid);
+        return helperFunction(a, key, comparator, low, mid - 1, mid);
       } else if (comparisonValue < 0) {
-        low = mid + 1;
+        return helperFunction(a, key, comparator, mid + 1, high, keyIndex);
       } else {
-        high = mid - 1;
+        return helperFunction(a, key, comparator, low, mid - 1, keyIndex);
       }
-
     }
-    return initialKeyIndex;
+
+    return keyIndex;
   }
 
   // Versions of the above functions that use the natural ordering of the type T.
